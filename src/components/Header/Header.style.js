1d15424f75322e099dccf2b0b01a74b8fc85dec4
vahidGhadiri <whydrf@gmail.com>
2021-10-28 16:23:24 +0330

    [Feature] Add Header component

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -4,13 +4,13 @@ export const Container = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 10px 20px;
+  padding: 1.5rem 2rem;
   width: 100%;
-  background-color: red;
   position: sticky;
   top: 0;
   z-index: 100;
   font-size: 2rem;
   font-family: "Pacifico", sans-serif;
   white-space: nowrap;
+  border-bottom: 1px solid #cecece;
 `
